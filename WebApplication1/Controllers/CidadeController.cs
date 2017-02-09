78e816582db28f97fa6a55ff8d7c28e281037b96
lhyngrha <lhyngrha@hotmail.com>
2017-02-09 10:17:48 -0300

    Servico de Estado e Cidade implementados e publicados 20131011110142

---
+++ WebApplication1/Controllers/CidadeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class CidadeController : ApiController
+    {
+
+        public IEnumerable<Models.Cidade> Get()
+        {
+            Models.BancoDataContext dc = new Models.BancoDataContext();
+            var r1 = from r in dc.Cidades orderby r.Nome select r;
+            return r1.ToList();
+        }
+
+        public void Post([FromBody] Models.Cidade cidade)
+        {
+            Models.BancoDataContext dc = new Models.BancoDataContext();
+            dc.Cidades.InsertOnSubmit(new Models.Cidade() { Nome = cidade.Nome, IdEstado = cidade.IdEstado });
+            dc.SubmitChanges();
+        }
+
+        public void Put(int id, [FromBody] Models.Cidade cidade)
+        {
+            Models.BancoDataContext dc = new Models.BancoDataContext();
+            Models.Cidade cid = (from f in dc.Cidades where f.Id == id select f).Single();
+            cid.Nome = cidade.Nome;
+            cid.IdEstado = cidade.IdEstado;
+            dc.SubmitChanges();
+        }
+
+        public void Delete(int id)
+        {
+            Models.BancoDataContext dc = new Models.BancoDataContext();
+            Models.Cidade r = (from f in dc.Cidades where f.Id == id select f).Single();
+            dc.Cidades.DeleteOnSubmit(r);
+            dc.SubmitChanges();
+        }
+
+    }
+}
---
+++ WebApplication1/Controllers/EstadoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class EstadoController : ApiController
+    {
+        public IEnumerable<Models.Estado> Get()
+        {
+            Models.BancoDataContext dc = new Models.BancoDataContext();
+            var r1 = from r in dc.Estados orderby r.Sigla select r;
+            return r1.ToList();
+        }
+    }
+}
