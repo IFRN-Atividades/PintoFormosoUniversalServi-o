c2affe7fd5b0c5b1f98e61c35f97c72c1fe0f9fa
Gilson Gabriel <Gilson Gabriel>
2017-02-11 13:45:51 -0300

    Adicionado controller usuario - FALTA PUBLICAR O SERVICO

--- WebApplication1/Controllers/UsuarioController.cs
+++ WebApplication1/Controllers/UsuarioController.cs
@@ -9,5 +9,27 @@ namespace WebApplication1.Controllers
 {
     public class UsuarioController : ApiController
     {
+        public IEnumerable<Models.Usuario> Get()
+        {
+            Models.BancoDataContext dc = new Models.BancoDataContext();
+            var listaUsuarios = from f in dc.Usuarios select f;
+            return listaUsuarios.ToList();
+        }
+
+        public void Post([FromBody]Models.Usuario usuario)
+        {
+            Models.BancoDataContext dc = new Models.BancoDataContext();
+            dc.Usuarios.InsertOnSubmit(new Models.Usuario() { Id = usuario.Id, Nome = usuario.Nome, Senha = usuario.Senha });
+            dc.SubmitChanges();
+        }
+
+        // DELETE api/values/5]
+        public void Delete(int Id)
+        {
+            Models.BancoDataContext dc = new Models.BancoDataContext();
+            var usuario = (from f in dc.Usuarios where f.Id == Id select f).Single();
+            dc.Usuarios.DeleteOnSubmit(usuario);
+            dc.SubmitChanges();
+        }
     }
 }
